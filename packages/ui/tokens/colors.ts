0eaf19872f41e2bad7527dc3c96c83a91d8fbb51
Oswaldo Tedesco <oter2901@gmail.com>
2023-06-22 16:06:55 -0400

    add css variables and templating

---
+++ packages/ui/tokens/colors.ts
@@ -0,0 +1,15 @@
+export const baseColors = {}
+
+export const additionalColors = {}
+
+export const lightColors = {
+  ...baseColors,
+  ...additionalColors,
+  cardBorder: '#E7E3EB',
+}
+
+export const darkColors = {
+  ...baseColors,
+  ...additionalColors,
+  cardBorder: '#383241',
+}
